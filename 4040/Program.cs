0ce95c10c48f95e5e5ebc19c55e15355eb3be72f
Obbeen <anton.obom92@gmail.com>
2020-09-21 11:14:42 +0200

    la till 4040

---
+++ 4040/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

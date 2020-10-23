ad975ff8e582652235305ba967d1441486c0a44c
Obbeen <anton.obom92@gmail.com>
2020-10-23 15:09:06 +0200

    började på del 3

---
+++ 4630/Userinterface.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace _4630 
+{   
+    class UserInterface
+    {
+        public void Run()
+        {
+            Console.WriteLine("1. lägg till ny anställd.");
+            Console.WriteLine("2. skriv ut uppgifter om anställda");
+            Console.WriteLine("3. skriv ut namn på alla an");
+            Console.WriteLine("4. ändra uppgifter om anställda");
+            Console.WriteLine("a. Avsluta");
+        }
+    }
+}
--- Extrauppgift/Program.cs
+++ Extrauppgift/Program.cs
@@ -13,7 +13,7 @@ namespace Extrauppgift
             List<QuestionCard> questions = new List<QuestionCard>();
            
              List<QuestionCard> fragor = new List<QuestionCard>();
-            questions.Add(new QuestionChoice("Vad heter Norges hvudstad?", new List<string>(new string[] { "Oslo", "Stockholm", "Berlin", "London" }), "1"));
+            questions.Add(new QuestionChoice("Vad heter Norges hvudstad?", new List<string>(new string[] { "Oslo", "Stockholm", "Berlin", "London" }), "    1"));
             questions.Add(new QuestionChoice("Vad heter Danmarks huvudstad?", new List<string>(new string[] { "Oslo", "Köpenhamn", "New York", "London" }), "2"));
             questions.Add(new QuestionChoice("vad heter Finlands huvudstad?", new List<string>(new string[] { "Alingsås", "Stockholm", "Helsingfors", "Göteborg" }), "3"));
 

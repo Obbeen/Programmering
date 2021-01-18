14743247e321a5530eecb417e071eb2ab2c21ae9
Obbeen <anton.obom93@gmail.com>
2021-01-18 11:01:29 +0100

    3434

---
+++ DrillWpfObjectRunner-master/DrillWpfObject/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrillWpfObject
+{
+    class Result
+    {
+        // write code here
+        private int distance;
+
+        private string name;
+
+        private int time;
+
+
+
+        public Result(string time, string name, string distance)
+        {
+            this.time = Convert.ToInt32(time);
+            this.distance =Convert.ToInt32(distance);
+            this.name = name;
+        }
+         public string tostring()
+        {
+            string text = "" ;
+            text = text + distance + Environment.NewLine;
+            text = text + name + Environment.NewLine;
+            text = text + time + Environment.NewLine;
+            return text;
+
+        }
+    }
+}
